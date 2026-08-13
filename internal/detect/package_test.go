5cfe30271baa9f3f058c22b5c3ea8444f78b2a02
Renato Quinhoneiro Todorov <renato.todorov@gmail.com>
2026-08-13 11:29:18 +0200

    feat(detect): baggage millisecond reader, int or digit-string

---
+++ internal/detect/package_test.go
@@ -0,0 +1,14 @@
+// Copyright The retrosampler Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package detect
+
+import (
+	"testing"
+
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
